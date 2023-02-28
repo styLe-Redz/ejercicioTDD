311510a2748b42cc3b0b87363c35b4afee9e9d95
Ubuntu <ubuntu@ip-172-31-26-112.ec2.internal>
2023-02-28 20:05:41 +0000

    first commit

---
+++ testArregloMenor.py
@@ -0,0 +1,8 @@
+from arregloMenor import encontrar_minimo()
+
+def test_Encontrar_Minimo():
+    assert encontrar_minimo([1,2,3,4,5]) == 1
+    assert encontrar_minimo([80, 30, 40, 27]) == 27
+    assert encontrar_minimo([10, 15, 9, 3]) == 3
+    
+    
